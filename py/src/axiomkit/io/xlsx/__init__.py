eabb7b39092482be7a4c10718beb9bfe3ea644f6
FuqingZh <fu.qing.zhang.work@gmail.com>
2026-01-20 14:43:53 +0800

    Add XLSX export specification init files

---
+++ py/src/axiomkit/io/xlsx/__init__.py
@@ -0,0 +1,4 @@
+from .main import XlsxFormatter
+from .spec import SpecCellFormat
+
+__all__ = ["XlsxFormatter", "SpecCellFormat"]
---
+++ py/src/axiomkit/io/xlsx/spec/__init__.py
@@ -0,0 +1,11 @@
+from .cell import SpecCellBorder, SpecCellFormat
+from .sheet import SpecSheetHorizontalMerge, SpecSheetSlice
+from .xlsx import SpecXlsxReport
+
+__all__ = [
+    "SpecCellBorder",
+    "SpecCellFormat",
+    "SpecSheetHorizontalMerge",
+    "SpecSheetSlice",
+    "SpecXlsxReport",
+]
