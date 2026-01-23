83b815ec7e6ee33d12117e1d75efb685918e8ffb
FuqingZh <fu.qing.zhang.work@gmail.com>
2026-01-23 13:48:25 +0800

    优化 DEFAULT_XLSX_FORMATS 中的整数、十进制和科学格式，使用 _cls_base_fmt_spec 以保持一致性

--- py/src/axiomkit/io/xlsx/conf/default.py
+++ py/src/axiomkit/io/xlsx/conf/default.py
@@ -7,14 +7,16 @@ from typing import Literal
 from ..spec import SpecCellFormat
 
 _LIT_FMT_KEYS = Literal["text", "integer", "decimal", "scientific", "header"]
-_cls_base_fmt_spec = SpecCellFormat(font_name="Times New Roman", font_size=10, border=1)
+_cls_base_fmt_spec = SpecCellFormat(
+    font_name="Times New Roman", font_size=11, border=1, align="left", valign="vcenter"
+)
 
 DEFAULT_XLSX_FORMATS: Mapping[_LIT_FMT_KEYS, SpecCellFormat] = MappingProxyType(
     {
-        "text": _cls_base_fmt_spec.with_(align="left", valign="vcenter"),
-        "header": _cls_base_fmt_spec.with_(bold=True, align="center", valign="vcenter"),
-        "integer": SpecCellFormat(num_format="0", border=1),
-        "decimal": SpecCellFormat(num_format="0.0000", border=1),
-        "scientific": SpecCellFormat(num_format="0.00E+0", border=1),
+        "text": _cls_base_fmt_spec,
+        "header": _cls_base_fmt_spec.with_(bold=True, align="center"),
+        "integer": _cls_base_fmt_spec.with_(num_format="0"),
+        "decimal": _cls_base_fmt_spec.with_(num_format="0.0000"),
+        "scientific": _cls_base_fmt_spec.with_(num_format="0.00E+0"),
     }
 )
