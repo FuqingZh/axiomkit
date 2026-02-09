c727f7399a1f17fba6f1bcad23302f354996fe41
FuqingZh <fu.qing.zhang.work@gmail.com>
2026-02-09 16:48:00 +0800

    feat: add AK namespace facade for package exports

---
+++ r/R/namespace-ak.r
@@ -0,0 +1,37 @@
+#' Unified AK Namespace Facade
+#'
+#' `ak` is a lightweight facade that groups package exports by domain.
+#' Use `ak$stats$...` for statistics-related APIs.
+#'
+#' @format NULL
+#' @usage ak
+#' @export
+ak <- NULL
+
+.create_ak_namespace <- function(pkg = "axiomkit") {
+    e_ak <- new.env(parent = emptyenv())
+    e_stats <- new.env(parent = emptyenv())
+
+    c_stats_exports <- c(
+        "QuantMatrix",
+        "create_quant_matrix",
+        "normalize_axis_median_center"
+    )
+    for (c_name in c_stats_exports) {
+        makeActiveBinding(
+            c_name,
+            local({
+                c_export_name <- c_name
+                function() getExportedValue(pkg, c_export_name)
+            }),
+            e_stats
+        )
+    }
+
+    e_ak$stats <- e_stats
+
+    lockEnvironment(e_stats, bindings = TRUE)
+    lockEnvironment(e_ak, bindings = TRUE)
+
+    e_ak
+}
--- r/R/zzz.R
+++ r/R/zzz.R
@@ -1,3 +1,11 @@
 .onLoad <- function(libname, pkgname) {
-  invisible(NULL)
+    e_ns <- asNamespace(pkgname)
+
+    if (exists("ak", envir = e_ns, inherits = FALSE) && bindingIsLocked("ak", e_ns)) {
+        unlockBinding("ak", e_ns)
+    }
+    assign("ak", .create_ak_namespace(pkg = pkgname), envir = e_ns)
+    lockBinding("ak", e_ns)
+
+    invisible(NULL)
 }
