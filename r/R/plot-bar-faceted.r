420ff8633ad97c75a1ca2b833e5670b0602706b5
FuqingZh <fu.qing.zhang.work@gmail.com>
2026-02-09 16:48:00 +0800

    feat: add faceted bar chart plotting function

--- r/R/plot-bar-faceted.r
+++ r/R/plot-bar-faceted.r
@@ -1,18 +1,29 @@
+#' Plot Faceted Bar Chart
+#'
+#' @param df A tabular object.
+#' @param col_x Column name used for x-axis.
+#' @param col_y Column name used for y-axis.
+#' @param col_y_name Column name used for facet label.
+#'
+#' @return Currently returns `NULL` (placeholder implementation).
+#' @importFrom data.table as.data.table
+#' @importFrom cli cli_abort
+#' @export
 plot_bar_faceted <- function(
   df,
   col_x = "X",
   col_y = "Y",
   col_y_name = "YName"
 ) {
-    dt <- data.table::as.data.table(df)
+    dt <- as.data.table(df)
     c_cols_dt <- colnames(dt)
     c_cols_args <- c(col_x, col_y, col_y_name)
     c_cols_inner <- intersect(c_cols_dt, c_cols_args)
     if (length(c_cols_inner) == 0) {
-        cli::cli_abort("None of the specified columns found in ``df``.")
+        cli_abort("None of the specified columns found in ``df``.")
     }
     if (!all(c(col_x, col_y) %in% c_cols_inner)) {
-        cli::cli_abort("All of ``col_x`` or ``col_y`` must be found in ``df``.")
+        cli_abort("All of ``col_x`` or ``col_y`` must be found in ``df``.")
     }
     
 }
