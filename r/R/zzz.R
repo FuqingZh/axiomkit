ba3187a98aa9ce522dc8c24a9eb589072ac3f980
FuqingZh <fu.qing.zhang.work@gmail.com>
2026-02-10 11:17:47 +0800

    fix: Improve package loading and namespace management in .onLoad

--- r/R/zzz.R
+++ r/R/zzz.R
@@ -1,10 +1,19 @@
 .onLoad <- function(libname, pkgname) {
     e_ns <- asNamespace(pkgname)
 
-    if (exists("ak", envir = e_ns, inherits = FALSE) && bindingIsLocked("ak", e_ns)) {
+    if (
+        exists("ak", envir = e_ns, inherits = FALSE) &&
+            bindingIsLocked("ak", e_ns)
+    ) {
         unlockBinding("ak", e_ns)
     }
-    assign("ak", .create_ak_namespace(pkg = pkgname), envir = e_ns)
+    obj_make_ak_namespace <- get(
+        ".create_ak_namespace",
+        mode = "function",
+        envir = e_ns,
+        inherits = FALSE
+    )
+    assign("ak", obj_make_ak_namespace(pkg = pkgname), envir = e_ns)
     lockBinding("ak", e_ns)
 
     invisible(NULL)
