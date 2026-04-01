3899257a38ff2b50175497d2e05983c91b69025f
FuqingZh <fu.qing.zhang.work@gmail.com>
2026-04-01 11:10:14 +0800

    Refactors t-test result schemas

--- python/axiomkit/stats/t_test/constant.py
+++ python/axiomkit/stats/t_test/constant.py
@@ -19,6 +19,13 @@ COLS_STATS_ONE_SAMPLE_NUMERIC = (
     "N",
 )
 
+SCHEMA_T_TEST_STATS: SchemaDict = {
+    "MeanDiff": pl.Float64,
+    "TStatistic": pl.Float64,
+    "DegreesFreedom": pl.Float64,
+    "PValue": pl.Float64,
+    "PAdjust": pl.Float64,
+}
 SCHEMA_T_TEST_TWO_SAMPLE_RESULT: SchemaDict = {
     "ContrastId": pl.Array(pl.String, 2),
     "GroupTest": pl.String,
@@ -27,20 +34,10 @@ SCHEMA_T_TEST_TWO_SAMPLE_RESULT: SchemaDict = {
     "NGroupRef": pl.Int64,
     "MeanGroupTest": pl.Float64,
     "MeanGroupRef": pl.Float64,
-    "MeanDiff": pl.Float64,
-    "TStatistic": pl.Float64,
-    "DegreesFreedom": pl.Float64,
-    "PValue": pl.Float64,
-    "PAdjust": pl.Float64,
-}
+} | SCHEMA_T_TEST_STATS
 
 SCHEMA_T_TEST_ONE_SAMPLE_RESULT: SchemaDict = {
     "N": pl.Int64,
     "Mean": pl.Float64,
     "PopMean": pl.Float64,
-    "MeanDiff": pl.Float64,
-    "TStatistic": pl.Float64,
-    "DegreesFreedom": pl.Float64,
-    "PValue": pl.Float64,
-    "PAdjust": pl.Float64,
-}
+} | SCHEMA_T_TEST_STATS
